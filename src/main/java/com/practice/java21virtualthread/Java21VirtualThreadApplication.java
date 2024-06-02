878edda0bed2241e66b59d2c83c0fa3b66235f7d
hgpark <phg9587@naver.com>
2024-06-02 23:26:49 +0900

    init

---
+++ src/main/java/com/practice/java21virtualthread/Java21VirtualThreadApplication.java
@@ -0,0 +1,13 @@
+package com.practice.java21virtualthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java21VirtualThreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java21VirtualThreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/java21virtualthread/Java21VirtualThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.java21virtualthread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java21VirtualThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

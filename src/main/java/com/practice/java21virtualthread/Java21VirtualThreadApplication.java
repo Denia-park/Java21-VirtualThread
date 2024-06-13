56fc5a85b6d385dceb2ae596d9daf344ff710e39
hgpark <phg9587@naver.com>
2024-06-13 23:25:15 +0900

    virtual thread in task execution

--- src/main/java/com/practice/java21virtualthread/Java21VirtualThreadApplication.java
+++ src/main/java/com/practice/java21virtualthread/Java21VirtualThreadApplication.java
@@ -2,7 +2,9 @@ package com.practice.java21virtualthread;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class Java21VirtualThreadApplication {
 
